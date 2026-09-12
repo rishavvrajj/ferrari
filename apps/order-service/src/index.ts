0601e32d3aa34b37fb5401c0521209b78062ab67
rishavvrajj <rishav5raj3@gmail.com>
2026-09-12 18:38:43 +0530

    feat(services): add order product and payment services

---
+++ apps/order-service/src/index.ts
@@ -0,0 +1,21 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify();
+
+fastify.get('/health', (request, reply) => {
+    return reply.status(200).send({
+        status: "ok",
+        uptime: process.uptime(),
+        timestamp: Date.now()
+    })
+})
+
+const start = (async () => {
+    try {
+        await fastify.listen({ port: 8001 })
+        console.log("Order Service is running on 8001")
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+})();
---
+++ apps/payment-service/src/index.js
@@ -0,0 +1,24 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+const app = new Hono();
+app.get('/health', (c) => {
+    return c.json({
+        status: "ok",
+        uptime: process.uptime(),
+        timestamp: Date.now()
+    });
+});
+const start = (async () => {
+    try {
+        serve({
+            fetch: app.fetch,
+            port: 8002
+        }, (info) => {
+            console.log(`Payment Server is running on ${info.port}`);
+        });
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();

5388fe14e2db9506635b661e065ac73834217ee4
rishavvrajj <rishav5raj3@gmail.com>
2026-09-13 18:32:20 +0530

    feat(order-service): add Fastify service with authenticated test endpoint

--- apps/client/src/app/test/page.tsx
+++ apps/client/src/app/test/page.tsx
@@ -5,15 +5,24 @@ export default async function TestPage() {
     const { getToken } = await auth()
     const token = await getToken();
 
-    console.log(token)
-    const res = await fetch('http://localhost:8000/test', {
+    const resProduct = await fetch('http://localhost:8000/test', {
         headers: {
             authorization: `Bearer ${token}`
         },
     });
 
-    const data = await res.json()
-    console.log(data);
+    const dataProduct = await resProduct.json()
+    console.log(dataProduct);
+
+    const resOrder = await fetch('http://localhost:8001/test', {
+        headers: {
+            authorization: `Bearer ${token}`
+        },
+    });
+
+    const dataOrder = await resOrder.json()
+    console.log(dataOrder);
+
     return (
         <div>TestPage</div>
     )
--- apps/order-service/src/index.ts
+++ apps/order-service/src/index.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
+import { clerkPlugin, getAuth } from '@clerk/fastify'
 
 const fastify = Fastify();
+fastify.register(clerkPlugin);
 
 fastify.get('/health', (request, reply) => {
     return reply.status(200).send({
@@ -8,6 +10,14 @@ fastify.get('/health', (request, reply) => {
         uptime: process.uptime(),
         timestamp: Date.now()
     })
+});
+
+fastify.get('/test', (request, reply) => {
+    const { userId } = getAuth(request);
+    if (!userId) {
+        return reply.send({ message: "You are not logged In!" })
+    }
+    return reply.send({ message: "Order Service is authenticated!" })
 });
 
 const start = (async () => {
